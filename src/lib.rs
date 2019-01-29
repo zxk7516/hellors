fba25e29765c5d3c35bca60c59f5753a01ee4bba
zxk7516 <zxk7516@foxmail.com>
2019-01-29 17:37:48 +0800

    ddd

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
+use futures::Async;
+use futures::Future;
+
+pub struct Doubler<T> {
+    inner: T,
+}
+
+pub fn double<T>(inner: T) -> Doubler<T> {
+    Doubler { inner }
+}
+impl<T> Future for Doubler<T>
+where
+    T: Future<Item = usize>,
+{
+    type Item = usize;
+    type Error = T::Error;
+
+    fn poll(&mut self) -> Result<Async<usize>, T::Error> {
+        match self.inner.poll()? {
+            Async::Ready(v) => Ok(Async::Ready(v * 2)),
+            Async::NotReady => Ok(Async::NotReady),
+        }
+    }
+}
+
 pub fn add(a: i32, b: i32) -> i32 {
     a + b
 }
