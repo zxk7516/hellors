54f0feb8ee5f1582ea261460995d7ba91be449d2
zxk7516 <zxk7516@foxmail.com>
2019-01-29 18:04:03 +0800

    2

--- src/bin/a1.rs
+++ src/bin/a1.rs
@@ -2,6 +2,7 @@
 use tokio::fs;
 use tokio::io;
 use tokio::net::TcpListener;
+use tokio::net::TcpStream;
 use tokio::prelude::*;
 fn main() {
     let addr = "127.0.0.1:6142".parse().unwrap();
@@ -9,13 +10,23 @@ fn main() {
 
     let server = listener
         .incoming()
-        .for_each(|_socket| {
-            println!("{:?}", _socket);
+        .for_each(|socket: TcpStream| {
+            let (reader, writer) = socket.split();
+            let amount = io::copy(reader, writer);
+
+            let msg = amount.then(|result| {
+                match result {
+                    Ok((amount, _, _)) => println!("wrote {} bytes", amount),
+                    Err(e) => println!("error: {}", e),
+                }
+
+                Ok(())
+            });
+
+            tokio::spawn(msg);
             Ok(())
         })
-        .map_err(|err| {
-            println!("accept error = {:?}", err);
-        });
+        .map_err(|_err| ());
 
     println!("server running on localhost:6142");
 
