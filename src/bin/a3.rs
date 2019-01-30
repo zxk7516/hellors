bbcf856d1fb47b3dd7d475a1fb8dfbf12f9c40bd
zxk7516 <zxk7516@foxmail.com>
2019-01-30 13:58:42 +0800

    dd

--- src/bin/a3.rs
+++ src/bin/a3.rs
@@ -1,5 +1,6 @@
 #![allow(warnings)]
-#![feature(mpsc_select)]
+#[macro_use]
+extern crate crossbeam_channel;
 
 use crossbeam_channel::bounded;
 use crossbeam_utils::thread;
@@ -10,8 +11,20 @@ fn main() {
 
     let seek = |name, s, r| {
         select! {
-            recv(r) -> peer => println!("{} received a message from", name, peer.unwrap()),
-            send(s,name) -> _ =>{},
+            recv(r) -> peer => println!("{} received a message from {}.", name, peer.unwrap()),
+            send(s, name) -> _ => {},
         }
     };
+
+    thread::scope(|scope| {
+        for name in people {
+            let (s, r) = (s.clone(), r.clone());
+            scope.spawn(move |_| seek(name, s, r));
+        }
+    })
+    .unwrap();
+
+    if let Ok(name) = r.try_recv() {
+        println!("No one received {}’s message.", name);
+    }
 }
